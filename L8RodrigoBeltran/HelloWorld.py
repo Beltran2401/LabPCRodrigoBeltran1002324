f3092ac55ab6566eebf979db24e73774bed7251e
Beltran2401 <crbeltran@correo.url.edu.gt>
2024-03-12 11:47:46 -0600

    Eliminación del archivo del Lab8
    Se eliminó, ya que se creo una carpeta en la que estarán cotenidos todos los archivos de código

--- L8RodrigoBeltran/HelloWorld.py
+++
@@ -1,7 +0,0 @@
-nombre = input("Ingrese su nombre \n")
-print ("Hola", nombre)
-
-numero1 = int(input("Ingrese un número \n"))
-numero2 = int(input("Ingrese otro número \n"))
-
-print ("Suma:", numero1 + numero2)
