1119df9e503b6048d770fa19e4c55df63620c867
Beltran2401 <crbeltran@correo.url.edu.gt>
2024-03-14 20:30:51 -0600

    actualización lab09
    Código del Ejercicio #3 y correciones en general

--- Archivos Python/L9RodrigoBeltran1002324.py
+++ Archivos Python/L9RodrigoBeltran1002324.py
@@ -1,4 +1,4 @@
-#EJERCICIO 1
+#EJERCICIO 1 - ACTIVIDAD NO. 02
 print("Ejercicio 1: operaciones aritméticas")
 
 #Entradas
@@ -7,7 +7,7 @@ numero2 = int(input("Ingrese otro número entero: "))
 
 #Operaciones
 suma = numero1 + numero2
-resta = numero1 + numero2
+resta = numero1 - numero2
 multiplicacion = numero1 * numero2
 divisionEntera = numero1 // numero2
 divisionModular = numero1 % numero2
@@ -19,13 +19,13 @@ print(numero1, " * ", numero2, " = ", multiplicacion)
 print(numero1, " // ", numero2, " = ", divisionEntera)
 print(numero1, " % ", numero2, " = ", divisionModular)
 
-#EJERCICIO 2
-print("Ejercicio 2: operaciones booleanas")
+#EJERCICIO 2 - ACTIVIDAD NO. 02
+print("\nEjercicio 2: operaciones booleanas")
 
 #Operaciones
 mayor = numero1 > numero2
 menor = numero1 < numero2
-igualdad = numero2 == numero2
+igualdad = numero2 == numero1
 diferencia = numero1 != numero2
 
 #Salidas
@@ -34,15 +34,46 @@ print(numero1, " < ", numero2, " = ", menor)
 print(numero1, " == ", numero2, " = ", igualdad)
 print(numero1, " != ", numero2, " = ", diferencia)
 
-#EJERCICIO 3
-print("Ejercicio 3: jerarquía de operadores")
+#EJERCICIO 3 - ACTIVIDAD NO. 02
+print("\nEjercicio 3: jerarquía de operadores")
 
 #Entradas
 a = int(input("Ingrese un primer número entero: "))
 b = int(input("Ingrese un segundo número entero: "))
 c = int(input("Ingrese un tercer número entero: "))
 
+#Operaciones
 oper1 = (a*b) + c
 oper2 = (b+c) * a
 oper3 = a / (b+c)
 oper4 = ((3*a) + (2 * b)) / (c * c)
+
+#Salidas
+print("a * b + c =", oper1)
+print("a(b+c) =", oper2)
+print("a / (b + c) =", oper3)
+print("(3a + 2b) / (c^2) = ", oper4)
+
+#EJERCICIO - ACTIVIDAD NO. 03
+#Análisis: 
+
+#Encabezado
+print("\nCarlos Beltran - 1002324")
+
+#Entrada: Cantidad en m (metros)
+metros = float(input("Ingrese una cantidad en metros (m): "))
+
+#Proceso: Multiplicar o dividir la cantidad en metros por su respectivo factor de conversión, 
+#para obtener el resultado en millas, kilometros, pies y pulgadas.
+
+millas = metros / 1609
+kilometros = metros / 1000
+pies = metros * 3.28084
+pulgadas = metros * 39.3701
+
+#Salida: Cantidad en millas (mi), kilometros (km), pies (ft) y pulgadas (in)
+print("Resultado: \n")
+print("Millas:", millas)
+print ("Kilometros:", kilometros)
+print("Pies:", pies)
+print("Pulgadas:", pulgadas)
